5a11dd3dbf797e9d5632eb5843dc4544144f8330
Mitchell Snoek <93310900+Mitchell-Snoek@users.noreply.github.com>
2023-01-17 10:53:48 +0100

    updated database

--- backend/blokkenschema.sql
+++ backend/blokkenschema.sql
@@ -1,83 +1,50 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Gegenereerd op: 17 jan 2023 om 10:00
--- Serverversie: 10.4.21-MariaDB
--- PHP-versie: 8.0.10
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `festival`
---
-
--- --------------------------------------------------------
-
---
--- Tabelstructuur voor tabel `blokkenschema`
---
-
+DROP TABLE IF EXISTS `blokkenschema`;
 CREATE TABLE `blokkenschema` (
-  `Id` int(11) NOT NULL,
-  `Workshop_naam` varchar(200) NOT NULL,
-  `Spreker` varchar(200) NOT NULL,
-  `Ruimte` varchar(200) NOT NULL,
-  `Start_tijd` varchar(50) NOT NULL,
-  `Eind_tijd` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `id` mediumint(9) NOT NULL,
+  `Workshop_naam` varchar(100) NOT NULL,
+  `Spreker` varchar(100) DEFAULT NULL,
+  `Ruimte` varchar(100) NOT NULL,
+  `Start_tijd` time NOT NULL,
+  `Eind_tijd` time NOT NULL,
+  `url_programma` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `blokkenschema`
 --
 
-INSERT INTO `blokkenschema` (`Id`, `Workshop_naam`, `Spreker`, `Ruimte`, `Start_tijd`, `Eind_tijd`) VALUES
-(2, 'Welkom en informele start', 'Karoline Wiegerink', '1', '13:45', '14:00'),
-(3, 'Plenaire Kick-Off dagvoorzitter & Alle festival themas worden kort gepitcht', 'Arjen van Hijum', '2', '14:00', '14:45'),
-(4, 'Workshop: De Gouden Regels voor Klantvriendelijkheid in de toekomst', 'Bas Peters en Gerrit Piksen', '8', '14:45', '15:20'),
-(5, 'Workshop: Creatief met CX', 'Babs Asselbergs', '5', '14:45', '15:20'),
-(6, 'Themasessie: Van beleving naar betekenis (case Betaald Voetbal)', 'Themasessie: Van beleving naar betekenis (case Betaald Voetbal)', '3', '14:45', '15:20'),
-(7, 'Themasessie: De toekomst van klantgerichte organisaties in 280 tekens', 'Daan Noordeloos (Due North Consultancy), Evert Vols (Buro Improof), Stephan van Slooten (Altuïtion) en Kees Kerkvliet (TOTE-M) ', '2', '14:45', '15:20'),
-(8, 'Themasessie: De data gedreven toekomst (Case UWV: Verantwoorde toezicht algoritmes)', 'Robin Philippo (UWV) en Riens Koopman (Cmotions) en Serge Poppes (Pegamento)', '3', '15:20', '15:55'),
-(9, 'Workshop Ecosysteem Mapping ', 'Stein Wetzer en Wim Rampen (Livework Studio)', '2', '15:20', '15:55'),
-(10, 'Verdieping: Doorpraten over de klantgerichte organisatie van de toekomst met AkzoNobel', 'AkzoNobel onder leiding van Daan Noordeloos (Due North Consultancy) en Stephan van Slooten (Altuïtion)', '4', '15:20', '15:55'),
-(11, 'Workshop: De Gouden Regels voor Klantvriendelijkheid in de toekomst', 'Bas Peters en Gerrit Piksen (MarketResponse)', '1', '16:20', '16:55'),
-(12, 'Themasessie: De data gedreven toekomst (case Florius)', 'Deliane Schimmel (Florius) en Kees Groenewoud (Cmotions)', '5', '16:20', '16:55'),
-(13, 'Themasessie: Van beleving naar betekenis (case participatiesamenleving) ', 'Rob Beltman (H3ROES) en Jasper Klapwijk (Kantelingen)', '6', '16:20', '16:55'),
-(14, 'Themasessie Van klantrelaties naar ecosystemen | De digitale klantreis, een randvoorwaarde voor je ecosysteem', 'Gerard Datema (excap)', '2', '16:55', '17:30'),
-(15, 'Workshop: Van beleving naar betekenis: de BossUp! game', 'Bas Berkhout (Chemistry Team)', '2', '16:55', '17:30'),
-(16, 'Conclusies en knallend einde door dagvoorzitter', 'Arjen van Hijum', '1', '17:30', '18:00');
+INSERT INTO `blokkenschema` (`id`, `Workshop_naam`, `Spreker`, `Ruimte`, `Start_tijd`, `Eind_tijd`, `url_programma`) VALUES
+(1, 'Welkom in een winterse sfeer', 'PVKO', '1', '13:00:00', '13:45:00', ''),
+(2, 'Kick-Off plenair', 'Arjan van Hijum', '1', '14:00:00', '14:45:00', ''),
+(3, 'Van klantrelaties naar ecosysteem (case KVK) ', 'Fabian', '1', '14:45:00', '15:20:00', 'https://www.pvko.nl/bijeenkomsten/over/325/pvko-winterfestival-2023/schedule#activity-4'),
+(5, 'Data gedreven toekomst (Case UWV)', 'Fabian', '1', '15:20:00', '15:55:00', 'https://www.pvko.nl/bijeenkomsten/over/325/pvko-winterfestival-2023/schedule#activity-5'),
+(6, 'Pauze', 'Fabian', '1', '15:55:00', '16:20:00', ''),
+(7, 'Data gedreven toekomst (Case Florius)', 'Fabian', '1', '16:20:00', '16:55:00', 'https://www.pvko.nl/bijeenkomsten/over/325/pvko-winterfestival-2023/schedule#activity-7'),
+(8, 'Van klantrelaties naar ecosysteem (de digitale klantreis)', NULL, '1', '16:55:00', '17:30:00', 'https://www.pvko.nl/bijeenkomsten/over/325/pvko-winterfestival-2023/schedule#activity-8'),
+(9, 'Eindpresentatie Algemeen', 'Fabian', '1', '17:30:00', '18:00:00', ''),
+(10, 'Napraten met hap, drank en lach in het speelkwartier', 'Borrel', '1', '18:00:00', '20:00:00', ''),
+(11, 'Welkom in een winterse sfeer', 'PVKO', '2', '13:00:00', '13:30:00', NULL),
+(12, 'Kick-Off plenair', NULL, '2', '14:00:00', '14:45:00', NULL),
+(13, 'De toekomst van KG Org. in 280 tekens', NULL, '2', '14:45:00', '15:20:00', 'https://www.pvko.nl/bijeenkomsten/over/325/pvko-winterfestival-2023/schedule#activity-4'),
+(14, 'Tech-Touch balans van de toekomst', NULL, '2', '15:20:00', '15:55:00', 'https://www.pvko.nl/bijeenkomsten/over/325/pvko-winterfestival-2023/schedule#activity-4'),
+(15, 'Pauze', NULL, '2', '16:20:00', '16:55:00', 'https://www.pvko.nl/bijeenkomsten/over/325/pvko-winterfestival-2023/schedule#activity-7'),
+(16, 'Tech-Touch Balans van de toekomst (deel 2)', NULL, '2', '16:20:00', '16:55:00', 'https://www.pvko.nl/bijeenkomsten/over/325/pvko-winterfestival-2023/schedule#activity-7'),
+(17, 'De toekomst van de KG Org. in 280 tekens', NULL, '2', '16:55:00', '17:30:00', NULL),
+(18, 'Workshop: De Gouden Regels voor Klantvriendelijkheid in de toekomst', 'Bas Peters en Gerrit Piksen', '8', '14:45', '15:20', NULL),
+(19, 'Workshop: Creatief met CX', 'Babs Asselbergs', '5', '14:45', '15:20', NULL),
+(20, 'Themasessie: Van beleving naar betekenis (case Betaald Voetbal)', 'Themasessie: Van beleving naar betekenis (case Betaald Voetbal)', '3', '14:45', '15:20', NULL),
+(21, 'Themasessie: De data gedreven toekomst (Case UWV: Verantwoorde toezicht algoritmes)', 'Robin Philippo (UWV) en Riens Koopman (Cmotions) en Serge Poppes (Pegamento)', '3', '15:20', '15:55', NULL),
+(22, 'Verdieping: Doorpraten over de klantgerichte organisatie van de toekomst met AkzoNobel', 'AkzoNobel onder leiding van Daan Noordeloos (Due North Consultancy) en Stephan van Slooten (Altuïtion)', '4', '15:20', '15:55', NULL),
+(23, 'Themasessie: De data gedreven toekomst (case Florius)', 'Deliane Schimmel (Florius) en Kees Groenewoud (Cmotions)', '5', '16:20', '16:55', NULL),
+(24, 'Themasessie: Van beleving naar betekenis (case participatiesamenleving) ', 'Rob Beltman (H3ROES) en Jasper Klapwijk (Kantelingen)', '6', '16:20', '16:55', NULL);
 
---
--- Indexen voor geëxporteerde tabellen
---
-
---
--- Indexen voor tabel `blokkenschema`
---
 ALTER TABLE `blokkenschema`
-  ADD PRIMARY KEY (`Id`);
+  ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT voor geëxporteerde tabellen
---
-
---
--- AUTO_INCREMENT voor een tabel `blokkenschema`
---
 ALTER TABLE `blokkenschema`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` mediumint(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
